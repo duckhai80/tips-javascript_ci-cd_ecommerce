bf5ba01566ebd9f99d1edd93141b0f882bda36da
Truong Duc Khai <duckhai80@gmail.com>
2026-05-26 23:03:28 +0700

    Add users api

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+
+router.get("/api/users", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+    metadata: [
+      {
+        name: "Messi",
+        age: 37,
+        country: "Argentina",
+      },
+      {
+        name: "Ronaldo",
+        age: 40,
+        country: "Portugal",
+      },
+      {
+        name: "Neymar",
+        age: 32,
+        country: "Brazil",
+      },
+    ],
+  });
+});
 
 module.exports = router;
